26028ee035c0b2d31bc347e547c316498e23c6ca
Marta <martadabrowka@gmail.com>
2017-10-08 20:07:20 +0100

    added guest needs pay fee

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,11 +5,24 @@ require("../guest.rb")
 class TestGuest < MiniTest::Test
 
   def setup
-    @guest = Guest.new("Cosima")
+    @guest = Guest.new("Cosima", 50, "My Hips don't lie")
   end
 
   def test_guest_has_name()
     assert_equal("Cosima", @guest.name)
+  end
+
+  def test_guest_has_money()
+    assert_equal(50, @guest.money)
+  end
+
+  def test_guest_can_spend_money()
+    actual = @guest.remove_money(12)
+    assert_equal(38, @guest.money)
+  end
+
+  def test_guest_has_fave_song()
+    assert_equal("My Hips don't lie", @guest.favourite_song)
   end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,8 +7,9 @@ require_relative("../song.rb")
 class RoomTest < MiniTest::Test
 
   def setup()
-    @room = Room.new("Sound of Maritime")
-    @guest = Guest.new("Pablito")
+    @room = Room.new("Sound of Maritime", 6, 10, 0)
+    @guest = Guest.new("Pablito", 200000, "La donna e mobile")
+    @guest1 = Guest.new("Gustavo", 5, "La cucaracha")
     @guests = []
     @song = Song.new("My Heart Will Go On", "Celine Dion")
     @songs = []
@@ -18,28 +19,48 @@ class RoomTest < MiniTest::Test
       assert_equal("Sound of Maritime", @room.name)
   end
 
-   def test_check_room_has_no_guests()
-     assert_equal(0, @room.guest_count)
+    def test_check_room_has_no_guests()
+      assert_equal(0, @room.guest_count)
+    end
+
+    def test_guest_checked_in()
+      actual = @room.guest_check_in(@guest)
+      assert_equal(1, actual)
+    end
+
+    def test_guest_checked_out()
+      @guests << @guest
+      actual = @room.guest_check_out(@guest)
+      assert_equal(0, actual)
+    end
+
+   def test_room_has_no_songs()
+     assert_equal(0, @room.song_count)
    end
 
-   def test_guest_checked_in()
-     actual = @room.guest_check_in(@guest)
-     assert_equal(1, actual)
-   end
-
-   def test_guest_checked_out()
-     @guests << @guest
-     actual = @room.guest_check_out(@guest)
-     assert_equal(0, actual)
-   end
-
-  def test_room_has_no_songs()
-    assert_equal(0, @room.song_count)
-  end
-
-  def test_add_song_to_room()
+   def test_add_song_to_room()
     actual = @room.add_song(@song)
     assert_equal(1, actual)
   end
+
+   def test_guest_checked_in__takes_money()
+     @room.guest_check_in(@guest)
+     assert_equal(10, @room.till())
+   end
+
+   def test_guest_checked_in__cant_afford()
+     @room.guest_can_afford(@guest1)
+     assert_equal(0, @room.guest_count)
+   end
+
+   def test_room_has_capacity()
+     actual = @room.capacity()
+     assert_equal(6, actual)
+   end
+
+   def test_add_to_till()
+     actual = @room.add_to_till(6)
+     assert_equal(6, actual)
+   end
 
 end
