b99599583e8c39c3faf91a675030372aba8b5e12
Marta <martadabrowka@gmail.com>
2017-10-08 18:51:46 +0100

    guest check in and check out

--- room.rb
+++ room.rb
@@ -2,11 +2,24 @@ class Room
 
   attr_reader :name
 
-
   def initialize(name)
     @name = name
     @guests = []
     @songs = []
   end
+
+  def guest_count()
+    @guests.length()
+  end
+
+  def guest_check_in(new_guest)
+    @guests << new_guest
+    return guest_count
+  end
+
+ def guest_check_out(guest)
+   @guests.delete(guest)
+   return guest_count
+ end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,16 +1,34 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../room.rb")
-
+require_relative("../guest.rb")
 
 class RoomTest < MiniTest::Test
 
   def setup()
     @room = Room.new("Sound of Maritime")
+    @guest = Guest.new("Pablito")
+    @guests = []
   end
 
   def test_room_has_name()
       assert_equal("Sound of Maritime", @room.name)
-    end
+  end
+
+   def test_check_room_has_no_guests()
+     assert_equal(0, @room.guest_count)
+   end
+
+   def test_guest_checked_in()
+     actual = @room.guest_check_in(@guest)
+     assert_equal(1, actual)
+   end
+
+   def test_guest_checked_out()
+     @guests << @guest
+     actual = @room.guest_check_out(@guest)
+     assert_equal(0, actual)
+   end
+
 
 end
